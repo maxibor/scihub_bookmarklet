ca27434fec29198d0785e1ffaab944e4e8d28ed8
maxibor <maxime.borry@gmail.com>
2018-02-07 15:10:12 +0100

    added browser extension

--- browser_extension/content.js
+++ browser_extension/content.js
@@ -1,11 +1,13 @@
 (function(){
     let domain = 'tw';
+
     let links = document.getElementsByTagName('a');
     let scihub = 'http://sci-hub.'+domain+'/';
     for(var i = 0, l = links.length; i < l; i++) {
       let alink = links[i];
       alink.href = alink.href.replace(/^((http(s|):\/\/|www\.)|)(dx.|)doi\.org\//gim, scihub);
       alink.innerHTML = alink.innerHTML.replace(/^((http(s|):\/\/|www\.)|)(dx.|)doi\.org\//gim, scihub);
+
       console.log(alink.href)
    }
 })();
