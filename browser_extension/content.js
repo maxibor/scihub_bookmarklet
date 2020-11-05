bedfeaf595452d4c85b8136fe6a01277b5493add
maxibor <maxime.borry@gmail.com>
2020-11-05 12:48:08 +0100

    update extension

--- browser_extension/content.js
+++ browser_extension/content.js
@@ -1,9 +1,9 @@
-(function(){
-    let domain = 'tw';
+(function () {
+   let domain = 'se';
 
-    let links = document.getElementsByTagName('a');
-    let scihub = 'http://sci-hub.'+domain+'/';
-    for(var i = 0, l = links.length; i < l; i++) {
+   let links = document.getElementsByTagName('a');
+   let scihub = 'http://sci-hub.' + domain + '/';
+   for (var i = 0, l = links.length; i < l; i++) {
       let alink = links[i];
       alink.href = alink.href.replace(/^((http(s|):\/\/|www\.)|)(dx.|)doi\.org\//gim, scihub);
       alink.innerHTML = alink.innerHTML.replace(/^((http(s|):\/\/|www\.)|)(dx.|)doi\.org\//gim, scihub);
